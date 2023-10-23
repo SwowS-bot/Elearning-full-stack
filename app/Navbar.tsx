447fa1cb034c470bcd5ef26b6e7c8e5d1922a52d
thuan-NQT <92578416+SwowS-bot@users.noreply.github.com>
2023-10-23 14:54:59 +0700

    edit nav

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -13,13 +13,13 @@ const Navbar = () => {
         <Link href="/">Logo</Link>
       </div>
       <div className="flex items-center space-x-4">
-        <Link href="#" className="text-white">Courses</Link>
-        <Link href="#" className="text-white">Lectures</Link>
+        <Link href="/Courses" className="text-white">Courses</Link>
+        <Link href="/Lectures" className="text-white">Lectures</Link>
         <div className="relative group">
           <button onClick={() => setShowDropdown(!showDropdown)} className="text-white">Manage</button>
           {showDropdown && (
             <div className="absolute left-0 mt-2 w-32 bg-white text-black p-2 rounded-md flex flex-col">
-              <a href="#">Scores</a>
+              <a href="#">Courses</a>
               <a href="#">Progress</a>
             </div>
           )}
