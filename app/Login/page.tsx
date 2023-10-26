f1cf387af75cf60aab19af27ddbf89b3cc8cc9c1
thuan-NQT <92578416+SwowS-bot@users.noreply.github.com>
2023-10-26 18:27:11 +0700

    update

---
+++ app/Login/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-between p-24">
+      <div className=" bg-black p-8 rounded shadow-md w-96">
+        <h1 className=" text-center mb-8 text-4xl text-white">Login</h1>
+        <form>
+            
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,22 +1,33 @@
-'use client'
+"use client";
 
-import { useState } from 'react';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
 
   return (
-    <nav className="flex items-center justify-between p-6 bg-slate-500">
+    <nav className="flex items-center justify-between p-6 bg-black">
       <div>
-        <Link href="/">Logo</Link>
+        <Link href="/" className=" text-white">
+          Logo
+        </Link>
       </div>
       <div className="flex items-center space-x-4">
-        <Link href="/Courses" className="text-white">Courses</Link>
-        <Link href="/Lectures" className="text-white">Lectures</Link>
+        <Link href="/Courses" className="text-white">
+          Courses
+        </Link>
+        <Link href="/Lectures" className="text-white">
+          Lectures
+        </Link>
         <div className="relative group">
-          <button onClick={() => setShowDropdown(!showDropdown)} className="text-white">Manage</button>
+          <button
+            onClick={() => setShowDropdown(!showDropdown)}
+            className="text-white"
+          >
+            Manage
+          </button>
           {showDropdown && (
             <div className="absolute left-0 mt-2 w-32 bg-white text-black p-2 rounded-md flex flex-col">
               <a href="#">Courses</a>
@@ -25,13 +36,20 @@ const Navbar = () => {
           )}
         </div>
         {!showSearch ? (
-          <button onClick={() => setShowSearch(true)} className="text-white">Search</button>
+          <button onClick={() => setShowSearch(true)} className="text-white">
+            Search
+          </button>
         ) : (
-          <input type="text" placeholder="Search..." onBlur={() => setShowSearch(false)} className="p-1 rounded-md" />
+          <input
+            type="text"
+            placeholder="Search..."
+            onBlur={() => setShowSearch(false)}
+            className="p-1 rounded-md"
+          />
         )}
-        <button className="p-2 bg-white rounded-full text-blue-500">
-          Login
-        </button>
+        <Link href="/Login" className="p-2 bg-white rounded-xl text-blue-500">
+          Login/SignUp
+        </Link>
       </div>
     </nav>
   );
